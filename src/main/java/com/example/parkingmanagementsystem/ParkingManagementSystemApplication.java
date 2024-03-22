d72122e24428e6fbc8c9aa0ba84d94be76b58ea2
MariusB <92020282+BiraMarius@users.noreply.github.com>
2024-03-23 00:12:36 +0200

    Initial commit

---
+++ src/main/java/com/example/parkingmanagementsystem/ParkingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parkingmanagementsystem/ParkingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
